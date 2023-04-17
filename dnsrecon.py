cf08e5f15629ec45479f2d1c913817f3e7d361bf
Durruti <88114770+Durruuti@users.noreply.github.com>
2023-04-17 13:36:51 +0200

    dnsrecon.py updated
    He realizado algunos cambios en el código original de MIATA DNSRECON con el objetivo de optimizar la aplicación. Estos cambios han sido realizados por GPT-v3, un modelo de lenguaje de inteligencia artificial diseñado por OpenAI. Los cambios se centran en mejorar la eficiencia del código y hacerlo más legible.

--- dnsrecon.py
+++ dnsrecon.py
@@ -4,88 +4,87 @@ import sys
 import dns.resolver
 from datetime import datetime
 
-#Creacion variable splash
+
+# Creación de la variable splash
 splash = pyfiglet.figlet_format("MIATA DNSRECON")
 print(splash)
 
-#Conseguir la IP de nuestro dominio objetivo
+
+# Obtener la IP del dominio objetivo
 tdomain = input("Introduce el dominio: ")
 
-#Printear un banner sobre la busqueda del dominio
+# Mostrar un banner sobre la búsqueda del dominio
+print("_" * 60)
+print("Por favor espera, buscando la IP del dominio introducido...")
+print("_" * 60)
 
-print("_"* 60)
-print("Por favor espera, buscando la IP del dominio introducido")
-print("_"* 60)
-
-#Comprobar la fecha y el tiempo del scan
+# Comprobar la fecha y el tiempo del escaneo
 t1 = datetime.now()
-print("La dirección IP del host es: ", socket.gethostbyname(tdomain))
+ip_address = socket.gethostbyname(tdomain)
+print("La dirección IP del host es:", ip_address)
 
-#Comprobar el tiempo de nuevo
+# Comprobar el tiempo de nuevo
 t2 = datetime.now()
 
-#Calcular cuanto ha tardado el scan [t2 -t1]
-total = t2 -t1
-
+# Calcular cuánto ha tardado el escaneo (t2 - t1)
+total = t2 - t1
 print("Escaneo completado en:", total)
 
-#Creacion del escáner de puertos
 
+# Creación del escáner de puertos
 tserver = input("Introduce la IP de un host remoto para escanear: ")
-targetIP = socket.gethostbyname(tdomain)
 
-#Comprobar el tiempo tardado 
+# Comprobar el tiempo tardado
 t3 = datetime.now()
 print("_" * 60)
-print("Por favor espera, se está escaneando la IP por puertos abiertos ", targetIP)
+print("Por favor espera, se está escaneando la IP por puertos abiertos", ip_address)
 print("_" * 60)
 
-# Scaneo de la ip por puertos abiertos
+# Escaneo de la IP por puertos abiertos
 try:
-    for port in range (1, 65535):
+    for port in range(1, 65535):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #Establecer el timeout para que no este de por vida
-        socket.setdefaulttimeout(0.5)
-# Respuesta con la lista de puertos
-        result =s.connect_ex((targetIP, port))
+        # Establecer el timeout para que no esté de por vida
+        s.settimeout(0.5)
+        # Respuesta con la lista de puertos
+        result = s.connect_ex((ip_address, port))
         if result == 0:
-            print("Port {}[+]: Open",format(port))
+            print("Port {}: Open".format(port))
         s.close()
 
-# Crear mensaje de error en caso de ctrl+c
+# Crear mensaje de error en caso de Ctrl+C
 except KeyboardInterrupt:
-    print("Saliendo...")
+    print("\nSaliendo...")
 
-#Crear mensaje de error si no responde el host
+# Crear mensaje de error si no se puede conectar con el servidor
 except socket.error:
     print("No se pudo conectar con el server")
     sys.exit()
 
-#Comprobar el tiempo de nuevo
+# Comprobar el tiempo de nuevo
 t4 = datetime.now()
 
-#Calcular cuando ha tardado el scan en realizarse
+# Calcular cuánto ha tardado el escaneo en realizarse
 total = t4 - t3
-print("El escaneo se ha realizado en: ", total)
-
-#Enumeración DNS
+print("El escaneo se ha realizado en:", total)
 
 
-#Printear el banner
+# Enumeración DNS
 print("#" * 60)
 print("AHORA ENUMEREMOS")
 print("#" * 60)
 
 record_types = ["A", "AAAA", "NS", "CNAME", "MX"]
+
 try:
     domain = tdomain
 except IndexError:
     print("Error de sintaxis! ")
 
-for records in record_types:
+for record_type in record_types:
     try:
-        answer = dns.resolver.resolve(domain, records)
-        print(f"{records} Records")
+        answer = dns.resolver.resolve(domain, record_type)
+        print(f"{record_type} Records")
         print("_" * 60)
         for server in answer:
             print(server.to_text() + "\n")
@@ -94,5 +93,5 @@ for records in record_types:
     except dns.resolver.NXDOMAIN:
         print(f"{domain} no existe")
     except KeyboardInterrupt:
-        print("Saliendo... , Nos vemos !")
+        print("Saliendo...")
         quit()
